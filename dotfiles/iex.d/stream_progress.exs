9d3235d0b3fa3bd87e5b6e5e6c43a21270e0f844
Adrien Lucbert <adrien.lucbert@kbrwadventure.com>
2022-12-12 18:05:34 +0100

    fix(iex): StreamProgress.log failed on Task.async_stream
    doc(iex): StreamProgress.log documentation with examples

--- dotfiles/iex.d/stream_progress.exs
+++ dotfiles/iex.d/stream_progress.exs
@@ -1,4 +1,8 @@
 defmodule StreamProgress do
+  @moduledoc """
+  Stream progress logging utils.
+  """
+
   defp make_progress_bar(index, max, step, size) do
     0..size
     |> Enum.map(fn i -> if i / size <= index / (Float.round(max / step) * step), do: "█", else: "░" end)
@@ -9,10 +13,48 @@ defmodule StreamProgress do
   defp log_progress(%{index: i, max: max, step: step, time_elapsed: time_elapsed, time_left: time_left}) do
     IO.write("\r#{((i / max) * 100) |> Float.round(1)}% |#{make_progress_bar(i, max, step, 30)}| (#{i}/#{max}) [#{format_time(time_elapsed)}<#{format_time(time_left)}]")
   end
-  defp log_progress(%{i: i}), do: IO.puts("#{i}")
+  defp log_progress(%{index: i}), do: IO.puts("#{i}")
 
+  @doc """
+  Log stream progress.
+
+  Optional parameters:
+    - :max: size of the stream input (in case it can't be determined automatically)
+    - :step: log every nth element processed
+    - :logger: function used to log progress, of the form:
+      logger(%{index :: integer(), max :: integer(), step :: integer(), time_left :: integer()}) :: any()
+
+  If the stream input size can be determined (if stream.enum is an enumerable),
+  the :max and :step optional parameters will be determined automatically.
+  If the stream input size can't be determined and the :step optional parameter
+  is not set, :step will default to 1000, logging progress every 1000 elements
+  processed.
+
+  Usage example:
+    # The stream input is an enum from 1 to 1000, :max and :step can be determined automatically
+    iex(1)> 1..1000 |> Stream.map(fn _ -> :timer.sleep(1) end) |> StreamProgress.log() |> Stream.run()
+    100.0% |███████████████████████████████| (1000/1000) [00:02<00:00]:ok
+
+    # The stream input is a function (Stream.cycle), :max and :step can't be determined automatically, defaulting to 1000
+    iex(2)> 1..3 |> Stream.cycle |> Stream.take(10_000) |> StreamProgress.log() |> Stream.run()
+    1000
+    2000
+    3000
+    4000
+    5000
+    6000
+    7000
+    8000
+    9000
+    10000
+    :ok
+
+    # The stream input is a function (Task.async_stream), :max and :step can't be determined automatically, but are provided by the user
+    iex(3)> 1..1000 |> Task.async_stream(fn _ -> :timer.sleep(10) end, max_concurrency: 10) |> StreamProgress.log(max: 1000) |> Stream.run()
+    100.0% |███████████████████████████████| (1000/1000) [00:01<00:00]:ok
+  """
   def log(stream, opts \\ []) do
-    max = Keyword.get(opts, :max) || if is_function(stream.enum) do nil else stream.enum |> Enum.count end
+    max = Keyword.get(opts, :max) || if is_function(stream) || is_function(stream.enum) do nil else stream.enum |> Enum.count end
     step = Keyword.get(opts, :step) || if is_nil(max), do: 1000, else: max(1, div(max, 1000))
     logger = Keyword.get(opts, :logger, &log_progress/1)
     stream
@@ -23,7 +65,7 @@ defmodule StreamProgress do
         Map.get(state, :start_time) |> is_nil -> {[value], state |> Map.put(:start_time, now)}
         rem(idx, step) === 0  ->
           logger_opts = case max do
-            nil -> %{i: idx}
+            nil -> %{index: idx}
             max -> with elapsed <- now - state[:start_time], do: %{
               index: idx,
               max: max,
@@ -39,5 +81,3 @@ defmodule StreamProgress do
     end)
   end
 end
-
-# 1..1000 |> Stream.map(fn _ -> :timer.sleep(1) end) |> StreamProgress.log() |> Stream.run
